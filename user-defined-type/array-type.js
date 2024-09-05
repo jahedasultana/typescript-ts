8b593d60f4002d3dd73b85baaef9c03cc7782a32
jaheda8256 <jahedasultana47@gmail.com>
2024-09-05 16:02:47 +0600

    added multiplyType

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,8 @@
+// let userName: string[];
+var userName;
+userName = ['sathi', 'sumu', 'tanju'];
+console.log(userName[0]);
+userName.sort();
+console.log(userName);
+var multiplyTypes;
+multiplyTypes = [12, 'sathi'];
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,12 @@
+// let userName: string[];
+let userName: Array<string>;
+
+ userName = ['sathi', 'sumu', 'tanju'];
+
+ console.log(userName[0]);
+ userName.sort();
+ console.log(userName);
+
+ let multiplyTypes : (string | number)[];
+
+ multiplyTypes = [12, 'sathi'];
