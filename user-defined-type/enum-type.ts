c9afa483bd5abe5d556bae91522d4ac7b8b6d9fd
jaheda8256 <jahedasultana47@gmail.com>
2024-09-06 12:30:17 +0600

    added  string enum

--- user-defined-type/enum-type.ts
+++ user-defined-type/enum-type.ts
@@ -1,5 +1,5 @@
 // enum - store constants; duplicate value is not allowed
-//  enum types: numeric, string, hetergenous
+//  enum types: numeric, string, heterogenous
 //  numeric enum
 
 enum RequestType {
@@ -16,7 +16,16 @@ enum RequestType {
 enum RequestType2 {
     readData = 'Read-Data',
     deleteData = 'delete-data',
-    id = 101,
+
 }
 
 console.log(RequestType2['deleteData']);
+
+//  heterogenous enum
+enum RequestType3 {
+    readData = 'Read-Data',
+    deleteData = 'delete-data',
+    id = 101,
+}
+
+console.log(RequestType3['deleteData']);
